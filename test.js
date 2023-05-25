33eed291e4cd329901193cba52b0c105b137e1c6
lyn3nd <chisanayagi@gmail.com>
2023-05-25 21:34:57 +0200

    tab support added: fusion of all pages

--- test.js
+++ test.js
@@ -1,4 +1,78 @@
+//for the window to be draggable
+
 $( function() {
   $( "#draggable" ).draggable();
     handle: "title-bar"
 } );
+
+// -------------------
+
+// date time?
+
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+
+// -------------------
+
+//for the tabs to work
+
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll('[role="tab"]'); //selects all tabs
+  const tabList = document.querySelector('[role="tablist"]'); //selects all tablists
+
+  // Add a click event handler to each tab
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", changeTabs); //listens for clicks
+  });
+
+  // Enable arrow navigation between tabs in the tab list
+  let tabFocus = 0;
+
+  tabList.addEventListener("keydown", (e) => {
+    // Move right
+    if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
+      tabs[tabFocus].setAttribute("tabindex", -1);
+      if (e.key === "ArrowRight") {
+        tabFocus++;
+        // If we're at the end, go to the start
+        if (tabFocus >= tabs.length) {
+          tabFocus = 0;
+        }
+        // Move left
+      } else if (e.key === "ArrowLeft") {
+        tabFocus--;
+        // If we're at the start, move to the end
+        if (tabFocus < 0) {
+          tabFocus = tabs.length - 1;
+        }
+      }
+
+      tabs[tabFocus].setAttribute("tabindex", 0); //puts tab at front
+      tabs[tabFocus].focus(); //focuses on it
+    }
+  });
+});
+
+function changeTabs(e) {
+  const target = e.target;
+  const parent = target.parentNode;
+  const grandparent = parent.parentNode;
+
+  // Remove all current selected tabs
+  parent
+    .querySelectorAll('[aria-selected="true"]')
+    .forEach((t) => t.setAttribute("aria-selected", false));
+
+  // Set this tab as selected
+  target.setAttribute("aria-selected", true);
+
+  // Hide all tab panels
+  grandparent
+    .querySelectorAll('[role="tabpanel"]')
+    .forEach((p) => p.setAttribute("hidden", true));
+
+  // Show the selected panel
+  grandparent.parentNode
+    .querySelector(`#${target.getAttribute("aria-controls")}`)
+    .removeAttribute("hidden");
+}
